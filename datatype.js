ce64712eafaaf554daa3f1ecfa61060cb5d4e267
shivam sinha <77205094+shivam-0077@users.noreply.github.com>
2023-12-20 13:26:13 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+console.log(3+3);
+console.log("shivam"
+); // code readability should be high
+
+let name ="shivam"
+let age =23
+let isLoggedIn = false
+
+//  number=> 2 to power 53
+// bigint
+//  string=>""
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//  object
+console.log(typeof age);
---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId =12253
+let accountEmail ="sinha007shivam@gmail.com"
+var accountPassword ="12344"
+accountcity ="jaipur"
+
+// accountId =2 // not allowed
+let accountstate;
+/*
+
+*/
+
+
+console.log(accountId);
+accountEmail="sfds.com"
+accountPassword="242"
+accountcity="safddaqf"
+console.table([accountEmail,accountPassword,accountcity,accountstate])
